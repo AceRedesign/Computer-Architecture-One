f110eeb537207ec7b6fc8956ce2ad64227191d8d
aceredesign <steven.magadan@hotmail.com>
2018-04-12 21:54:23 -0700

    Steven Magadan - Added multiply and add actions

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,28 +1,32 @@
-const RAM = require('./ram');
-const CPU = require('./cpu');
-
+const RAM = require("./ram");
+const CPU = require("./cpu");
+const fs = require("fs");
+const readline = require("readline");
 /**
  * Load an LS8 program into memory
  *
  * TODO: load this from a file on disk instead of having it hardcoded
  */
 function loadMemory() {
+  let counter = 0;
+  const readLines = readline.createInterface({
+    input: fs.createReadStream(process.argv[2])
+  });
 
-    // Hardcoded program to print the number 8 on the console
-
-    const program = [ // print8.ls8
-        "10011001", // LDI R0,8  Store 8 into R0
-        "00000000",
-        "00001000",
-        "01000011", // PRN R0    Print the value in R0
-        "00000000",
-        "00000001"  // HLT       Halt and quit
-    ];
-
-    // Load the program into the CPU's memory a byte at a time
-    for (let i = 0; i < program.length; i++) {
-        cpu.poke(i, parseInt(program[i], 2));
+  readLines.on("line", line => {
+    // cpu.poke(counter, parseInt(line, 2));
+    // console.log('this is parseint: ', parseInt(line, 2));
+    // console.log(parseInt('123foo'));
+    let stringVar = line.split("#")[0].trim();
+    let parsing = parseInt(stringVar, 2);
+    if (!isNaN(parsing)) {
+      cpu.poke(counter++, parsing);
     }
+  });
+
+  readLines.on("close", line => {
+    cpu.startClock();
+  });
 }
 
 /**
@@ -34,6 +38,6 @@ let cpu = new CPU(ram);
 
 // TODO: get name of ls8 file to load from command line
 
-loadMemory(cpu);
+loadMemory(cpu, process.argv[2]);
 
-cpu.startClock();
+// cpu.startClock();
